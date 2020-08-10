c6b6179ab6ebfd955f15820d06eef5e3af3178c9
itplamen@gmail.com <itplamen@gmail.com>
2020-08-10 23:17:41 +0300

    Added logic for dealing cards

---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Contracts/ICardsDealer.cs
@@ -0,0 +1,9 @@
+﻿namespace SantaseCardGame.Core.Logic.Contracts
+{
+    using SantaseCardGame.Data.Models;
+
+    public interface ICardsDealer
+    {
+        void Deal(Deck deck, Player firstPlayer, Player secondPlayer);
+    }
+}
---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Dealing/CardsDealer.cs
@@ -0,0 +1,36 @@
+﻿namespace SantaseCardGame.Core.Logic.Dealing
+{
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+
+    public class CardsDealer : ICardsDealer
+    {
+        private const int NUMBER_OF_CARDS = 6;
+
+        public void Deal(Deck deck, Player firstPlayer, Player secondPlayer)
+        {
+            DealCards(deck, firstPlayer);
+            DealCards(deck, secondPlayer);
+
+            Card trumpCard = GetNextCard(deck);
+            deck.TrumpCard = trumpCard;
+        }
+
+        private void DealCards(Deck deck, Player player)
+        {
+            for (int i = 1; i <= NUMBER_OF_CARDS; i++)
+            {
+                Card card = GetNextCard(deck);
+                player.Cards.Add(card);
+            }
+        }
+
+        private Card GetNextCard(Deck deck)
+        {
+            Card card = deck.Cards[deck.Cards.Count - 1];
+            deck.Cards.RemoveAt(deck.Cards.Count - 1);
+
+            return card;
+        }
+    }
+}
