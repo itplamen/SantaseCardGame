6eaf5bf52e81653fdbe29e20a3948359b620ff3b
itplamen@gmail.com <itplamen@gmail.com>
2020-08-10 23:33:19 +0300

    Determine trick winner

---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Contracts/ITrickWinner.cs
@@ -0,0 +1,9 @@
+﻿namespace SantaseCardGame.Core.Logic.Contracts
+{
+    using SantaseCardGame.Data.Models;
+
+    public interface ITrickWinner
+    {
+        PlayerPosition GetWinner(Card firstPlayerCard, Card secondPlayerCard, CardSuit trumpSuit);
+    }
+}
---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Win/TrickWinner.cs
@@ -0,0 +1,23 @@
+﻿namespace SantaseCardGame.Core.Logic.Win
+{
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+
+    public class TrickWinner : ITrickWinner
+    {
+        public PlayerPosition GetWinner(Card firstPlayerCard, Card secondPlayerCard, CardSuit trumpSuit)
+        {
+            if (firstPlayerCard.Suit == secondPlayerCard.Suit)
+            {
+                return firstPlayerCard.Type > secondPlayerCard.Type ? PlayerPosition.First : PlayerPosition.Second;
+            }
+
+            if (secondPlayerCard.Suit == trumpSuit)
+            {
+                return PlayerPosition.Second;
+            }
+
+            return PlayerPosition.First;
+        }
+    }
+}
