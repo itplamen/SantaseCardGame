b8d48137085f0aaccbf68d18596d49663c9a8a98
itplamen@gmail.com <itplamen@gmail.com>
2020-08-12 01:09:21 +0300

    Added more data models

---
+++ SantaseCardGame/Data/SantaseCardGame.Data.Models/Announce.cs
@@ -0,0 +1,8 @@
+﻿namespace SantaseCardGame.Data.Models
+{
+    public enum Announce
+    {
+        Twenty = 20,
+        Forty = 40
+    }
+}
---
+++ SantaseCardGame/Data/SantaseCardGame.Data.Models/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace SantaseCardGame.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class Game
+    {
+        public Player FirstPlayer { get; set; }
+
+        public Player SecondPlayer { get; set; }
+
+        public Deck Deck { get; set; }
+
+        public List<Round> Rounds { get; set; } = new List<Round>();
+    }
+}
