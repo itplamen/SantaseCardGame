c049ad811680f4c040ac1c6dbcf0d9149647563f
itplamen@gmail.com <itplamen@gmail.com>
2020-08-12 01:09:49 +0300

    Added game manager

---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Contracts/IGameManager.cs
@@ -0,0 +1,9 @@
+﻿namespace SantaseCardGame.Core.Logic.Contracts
+{
+    using SantaseCardGame.Data.Models;
+
+    public interface IGameManager
+    {
+        Game StartGame(string username);
+    }
+}
---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Play/GameManager.cs
@@ -0,0 +1,49 @@
+﻿namespace SantaseCardGame.Core.Logic.Play
+{
+    using System.Collections.Generic;
+
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Contracts;
+    using SantaseCardGame.Data.Models;
+    
+    public class GameManager : IGameManager
+    {
+        private readonly ICardsDealer cardsDealer;
+        private readonly ICardsProvider cardsProvider;
+        private readonly ICardsShuffler cardsShuffler;
+
+        public GameManager(ICardsDealer cardsDealer, ICardsProvider cardsProvider, ICardsShuffler cardsShuffler)
+        {
+            this.cardsDealer = cardsDealer;
+            this.cardsProvider = cardsProvider;
+            this.cardsShuffler = cardsShuffler;
+        }
+
+        public Game StartGame(string username)
+        {
+            IEnumerable<Card> cards = cardsProvider.Get();
+            Deck deck = cardsShuffler.Shuffle(cards);
+
+            var firstPlayer = new Player()
+            {
+                Username = "Bot",
+                Position = PlayerPosition.First
+            };
+
+            var secondPlayer = new Player()
+            {
+                Username = username,
+                Position = PlayerPosition.Second
+            };
+
+            cardsDealer.Deal(deck, firstPlayer, secondPlayer);
+
+            return new Game()
+            {
+                Deck = deck,
+                FirstPlayer = firstPlayer,
+                SecondPlayer = secondPlayer
+            };
+        }
+    }
+}
