d496cc1c2d0e784a9d64d2e3159553b91377fe15
itplamen@gmail.com <itplamen@gmail.com>
2020-09-16 22:19:56 +0300

    Added logic for exchanging trump card for nine of trumps card

--- SantaseCardGame/Web/SantaseCardGame.Web.Client/Infrastructure/GameState.cs
+++ SantaseCardGame/Web/SantaseCardGame.Web.Client/Infrastructure/GameState.cs
@@ -2,11 +2,17 @@
 {
     using System;
 
+    using SantaseCardGame.Data.Models;
+
     public class GameState
     {
         public bool IsClosed { get; set; }
 
         public event Action OnCloseDeck;
+
+        public event Action<Card> OnChangeTrumpCard;
+
+        public event Action<Card> OnExchangeTrumpCardForNineOfTrumps;
 
         public void CloseDeck()
         {
@@ -14,6 +20,19 @@
             {
                 OnCloseDeck?.Invoke();
             }
+        }
+
+        public void ChangeTrumpCard(Card trumpCard)
+        {
+            if (!IsClosed)
+            {
+                OnChangeTrumpCard?.Invoke(trumpCard);
+            }
+        }
+
+        public void ExchangeTrumpCardForNineOfTrumps(Card nineOfTrumpsCard)
+        {
+            OnExchangeTrumpCardForNineOfTrumps?.Invoke(nineOfTrumpsCard);
         }
     }
 }
