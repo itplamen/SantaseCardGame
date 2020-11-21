f8de3f23fb1e24ad224ac0df4e5dd9b79d02c367
itplamen@gmail.com <itplamen@gmail.com>
2020-11-21 13:33:37 +0200

    Added player action models

---
+++ SantaseCardGame/Data/SantaseCardGame.Data.Models/PlayerAction.cs
@@ -0,0 +1,23 @@
+﻿namespace SantaseCardGame.Data.Models
+{
+    public class PlayerAction
+    {
+        public PlayerAction(PlayerActionType type, Announce announce)
+            :this(type, null, announce)
+        {
+        }
+
+        public PlayerAction(PlayerActionType type, Card card, Announce announce)
+        {
+            Type = type;
+            Card = card;
+            Announce = announce;
+        }
+
+        public PlayerActionType Type { get; }
+
+        public Card Card { get; }
+
+        public Announce Announce { get; }
+    }
+}
---
+++ SantaseCardGame/Data/SantaseCardGame.Data.Models/PlayerActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace SantaseCardGame.Data.Models
+{
+    public enum PlayerActionType
+    {
+        PlayCard = 1,
+        ChangeTrump = 2,
+        CloseDeck = 3,
+        Announce = 4
+    }
+}
