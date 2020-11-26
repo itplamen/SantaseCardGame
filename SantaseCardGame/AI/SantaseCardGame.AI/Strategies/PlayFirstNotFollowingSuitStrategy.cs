9505f007c6b84bc36bf11e8bb93c0770d1170019
itplamen@gmail.com <itplamen@gmail.com>
2020-11-26 23:30:06 +0200

    Added AI strategy for playin first when it is not required to follow the suit

---
+++ SantaseCardGame/AI/SantaseCardGame.AI/Strategies/PlayFirstNotFollowingSuitStrategy.cs
@@ -0,0 +1,32 @@
+﻿namespace SantaseCardGame.AI.Strategies
+{
+    using System.Collections.Generic;
+
+    using SantaseCardGame.AI.Contracts;
+    using SantaseCardGame.Data.Models;
+
+    public class PlayFirstNotFollowingSuitStrategy : IChoosePlayerActionStrategy
+    {
+        private readonly IEnumerable<IPlaySpecialFeature> playSpecialFeatures;
+
+        public PlayFirstNotFollowingSuitStrategy(IEnumerable<IPlaySpecialFeature> playSpecialFeatures)
+        {
+            this.playSpecialFeatures = playSpecialFeatures;
+        }
+
+        public PlayerAction ChoosePlayerAction(Player player)
+        {
+            foreach (var feature in playSpecialFeatures)
+            {
+                PlayerAction playerAction = feature.Play(player);
+
+                if (playerAction != null)
+                {
+                    return playerAction;
+                }
+            }
+
+            return new PlayerAction(PlayerActionType.None);
+        }
+    }
+}
