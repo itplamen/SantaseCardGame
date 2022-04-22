779f08d9097d8dda5cb5bfbad21ead270af63827
itplamen <itplamen@gmail.com>
2022-04-23 01:23:47 +0300

    Created in-memory data storage

---
+++ SantaseCardGame/Data/SantaseCardGame.Data/Contracts/IDataStorage.cs
@@ -0,0 +1,12 @@
+﻿namespace SantaseCardGame.Data.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface IDataStorage<TValue>
+        where TValue : class
+    {
+        Task Set(string key, TValue value);
+
+        Task<TValue> Get(string key);
+    }
+}
---
+++ SantaseCardGame/Data/SantaseCardGame.Data/InMemoryDataStorage.cs
@@ -0,0 +1,37 @@
+﻿namespace SantaseCardGame.Data
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Caching.Memory;
+
+    using SantaseCardGame.Data.Contracts;
+
+    public class InMemoryDataStorage<TValue> : IDataStorage<TValue> 
+        where TValue : class
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly MemoryCacheEntryOptions options;
+
+        public InMemoryDataStorage(IMemoryCache memoryCache, int expiration)
+        {
+            this.memoryCache = memoryCache;
+            this.options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(expiration);
+        }
+
+        public Task Set(string key, TValue value)
+        {
+            memoryCache.Set(key, value, options);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<TValue> Get(string key)
+        {
+            var cacheData = memoryCache.Get<TValue>(key);
+
+            return Task.FromResult(cacheData);
+        }
+    }
+}
