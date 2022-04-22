8fba1bdcfbcd5449abbe238523c562bfb26d1ca0
itplamen <itplamen@gmail.com>
2022-04-23 01:51:43 +0300

    Created trick winner

---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Contracts/ITrickWinner.cs
@@ -0,0 +1,11 @@
+﻿namespace SantaseCardGame.Core.Logic.Contracts
+{
+    using System.Collections.Generic;
+
+    using SantaseCardGame.Data.Models;
+
+    public interface ITrickWinner
+    {
+        PlayerPosition GetWinner(IEnumerable<KeyValuePair<PlayerPosition, Card>> cards, CardSuit trumpSuit);
+    }
+}
---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Winners/Trick/TrickWinner.cs
@@ -0,0 +1,34 @@
+﻿namespace SantaseCardGame.Core.Logic.Winners.Trick
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+
+    public class TrickWinner : ITrickWinner
+    {
+        public PlayerPosition GetWinner(IEnumerable<KeyValuePair<PlayerPosition, Card>> cards, CardSuit trumpSuit)
+        {
+            Card firstPlayed = cards.First().Value;
+            Card secondPlayed = cards.Last().Value;
+
+            if (firstPlayed.Suit == secondPlayed.Suit)
+            {
+                if (firstPlayed.Type > secondPlayed.Type)
+                {
+                    return cards.First().Key;
+                }
+
+                return cards.Last().Key;
+            }
+
+            if (secondPlayed.Suit == trumpSuit)
+            {
+                return cards.Last().Key;
+            }
+
+            return cards.First().Key;
+        }
+    }
+}
