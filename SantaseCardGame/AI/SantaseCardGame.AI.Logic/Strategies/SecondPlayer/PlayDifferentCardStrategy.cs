e6ced4d29b20d68f4848ce55abeb3add659b8dd9
itplamen <itplamen@gmail.com>
2022-04-24 11:25:34 +0300

    Created AI strategy for playing different second card when the AI player does not have cards from the same opponent/trump suit

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/SecondPlayer/PlayDifferentCardStrategy.cs
@@ -0,0 +1,33 @@
+﻿namespace SantaseCardGame.AI.Logic.Strategies.SecondPlayer
+{
+    using System.Linq;
+
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class PlayDifferentCardStrategy : BasePlayerActionStrategy
+    {
+        private readonly IDeckState deckState;
+
+        public PlayDifferentCardStrategy(ITrickState trickState, IDeckState deckState)
+            : base(trickState)
+        {
+            this.deckState = deckState;
+        }
+
+        protected override PlayerAction PlayStrategy(Player player)
+        {
+            if (player.Cards.All(x => x.Suit != OpponentCard.Suit))
+            {
+                Card card = player.Cards
+                    .Where(x => x.Suit != deckState.TrumpCard.Suit)
+                    .OrderBy(x => x.Type)
+                    .FirstOrDefault();
+
+                return new PlayerAction(PlayerActionType.PlayCard, card);
+            }
+
+            return new PlayerAction(PlayerActionType.None);
+        }
+    }
+}
