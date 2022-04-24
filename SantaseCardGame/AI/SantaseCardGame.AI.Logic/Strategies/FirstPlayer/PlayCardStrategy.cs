ec45ba37d3914fa5b6d62131aec468a5ac6abe4f
itplamen <itplamen@gmail.com>
2022-04-24 11:21:35 +0300

    Created AI strategy for playing first card

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/FirstPlayer/PlayCardStrategy.cs
@@ -0,0 +1,43 @@
+﻿namespace SantaseCardGame.AI.Logic.Strategies.FirstPlayer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class PlayCardStrategy : BasePlayerActionStrategy
+    {
+        private readonly IDeckState deckState;
+        private readonly IAnnouncementChecker announcementChecker;
+
+        public PlayCardStrategy(ITrickState trickState, IDeckState deckState, IAnnouncementChecker announcementChecker)
+            : base(trickState)
+        {
+            this.deckState = deckState;
+            this.announcementChecker = announcementChecker;
+        }
+
+        protected override PlayerAction PlayStrategy(Player player)
+        {
+            IEnumerable<Card> marriages = announcementChecker.GetMarriages(player.Cards);
+            IEnumerable<Card> playCards = player.Cards
+                .Where(x => !marriages.Contains(x))
+                .OrderBy(x => x.Type);
+
+            if (playCards.Any())
+            {
+                Card playCard = playCards.FirstOrDefault(x => x.Suit != deckState.TrumpCard.Suit);
+
+                if (playCard != null)
+                {
+                    return new PlayerAction(PlayerActionType.PlayCard, playCard);
+                }
+            }
+
+            return new PlayerAction(PlayerActionType.PlayCard, playCards.First());
+        }
+    }
+}
