179b81a230398fa0d1ffe4165492eeb6af6164ca
itplamen <itplamen@gmail.com>
2022-04-24 12:14:28 +0300

    Created AI logic for playing second card when it is required to follow suit

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/PlayLogics/SecondPlayer/PlaySecondFollowingSuitLogic.cs
@@ -0,0 +1,29 @@
+﻿namespace SantaseCardGame.AI.Logic.PlayLogics.SecondPlayer
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SantaseCardGame.AI.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class PlaySecondFollowingSuitLogic : BasePlayLogic
+    {
+        private readonly ITrickState trickState;
+        private readonly IDeckState deckState;
+
+        public PlaySecondFollowingSuitLogic(ITrickState trickState, IDeckState deckState, IEnumerable<IPlayerActionStrategy> strategies)
+            : base(trickState, strategies)
+        {
+            this.trickState = trickState;
+            this.deckState = deckState;
+        }
+
+        public override bool ShouldPlay(Player player)
+        {
+            return base.ShouldPlay(player) &&
+                deckState.ShouldFollowSuit &&
+                trickState.Cards.Any();
+        }
+    }
+}
