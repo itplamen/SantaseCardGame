5b5eb60d89504dcc8ca37d382c0f76345b2b3111
itplamen <itplamen@gmail.com>
2022-04-24 11:28:45 +0300

    Created AI strategy for playing higher second card

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/SecondPlayer/PlayHigherCardStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace SantaseCardGame.AI.Logic.Strategies.SecondPlayer
+{
+    using System.Linq;
+
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class PlayHigherCardStrategy : BasePlayerActionStrategy
+    {
+        public PlayHigherCardStrategy(ITrickState trickState)
+            : base(trickState)
+        {
+        }
+
+        protected override PlayerAction PlayStrategy(Player player)
+        {
+            Card card = player.Cards
+                .Where(x => x.Suit == OpponentCard.Suit)
+                .OrderByDescending(x => x.Type)
+                .FirstOrDefault(x => x.Type > OpponentCard.Type);
+
+            return new PlayerAction(PlayerActionType.PlayCard, card);
+        }
+    }
+}
