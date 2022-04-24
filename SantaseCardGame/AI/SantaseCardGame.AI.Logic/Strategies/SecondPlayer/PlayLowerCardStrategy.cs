a2c5c730ed843883912954e5eaec9e5085611b87
itplamen <itplamen@gmail.com>
2022-04-24 11:30:13 +0300

    Created AI strategy for playing lower second card

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/SecondPlayer/PlayLowerCardStrategy.cs
@@ -0,0 +1,25 @@
+﻿namespace SantaseCardGame.AI.Logic.Strategies.SecondPlayer
+{
+    using System.Linq;
+
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class PlayLowerCardStrategy : BasePlayerActionStrategy
+    {
+        public PlayLowerCardStrategy(ITrickState trickState)
+            : base(trickState)
+        {
+        }
+
+        protected override PlayerAction PlayStrategy(Player player)
+        {
+            Card card = player.Cards
+                .Where(x => x.Suit == OpponentCard.Suit)
+                .OrderBy(x => x.Type)
+                .FirstOrDefault(x => x.Type < OpponentCard.Type);
+
+            return new PlayerAction(PlayerActionType.PlayCard, card);
+        }
+    }
+}
