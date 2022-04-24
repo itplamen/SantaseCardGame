b8f3241a328e3f7aa17f65f5bc87da4e07571daa
itplamen <itplamen@gmail.com>
2022-04-24 12:12:32 +0300

    Created AI logic for playing first card when it is not required to follow suit

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/PlayLogics/FirstPlayer/PlayFirstNotFollowingSuitLogic.cs
@@ -0,0 +1,29 @@
+﻿namespace SantaseCardGame.AI.Logic.PlayLogics.FirstPlayer
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SantaseCardGame.AI.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class PlayFirstNotFollowingSuitLogic : BasePlayLogic
+    {
+        private readonly ITrickState trickState;
+        private readonly IDeckState deckState;
+
+        public PlayFirstNotFollowingSuitLogic(ITrickState trickState, IDeckState deckState, IEnumerable<IPlayerActionStrategy> strategies)
+            : base(trickState, strategies)
+        {
+            this.trickState = trickState;
+            this.deckState = deckState;
+        }
+
+        public override bool ShouldPlay(Player player)
+        {
+            return base.ShouldPlay(player) &&
+                !deckState.ShouldFollowSuit &&
+                !trickState.Cards.Any();
+        }
+    }
+}
