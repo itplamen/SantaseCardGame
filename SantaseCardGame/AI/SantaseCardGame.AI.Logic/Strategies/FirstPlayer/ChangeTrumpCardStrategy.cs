64a03a1d44a6a7e43e54a00d6bc5bb8aca0e2ea0
itplamen <itplamen@gmail.com>
2022-04-24 11:16:22 +0300

    Created AI strategy for changing trump card

---
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/FirstPlayer/ChangeTrumpCardStrategy.cs
@@ -0,0 +1,36 @@
+﻿namespace SantaseCardGame.AI.Logic.Strategies.FirstPlayer
+{
+    using System.Linq;
+
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class ChangeTrumpCardStrategy : BasePlayerActionStrategy
+    {
+        private readonly IDeckState deckState;
+        private readonly IPlayerActionValidator playerActionValidator;
+
+        public ChangeTrumpCardStrategy(ITrickState trickState, IDeckState deckState, IPlayerActionValidator playerActionValidator)
+            : base(trickState)
+        {
+            this.deckState = deckState;
+            this.playerActionValidator = playerActionValidator;
+        }
+
+        protected override PlayerAction PlayStrategy(Player player)
+        {
+            if (playerActionValidator.CanChangeTrump(player))
+            {
+                Card nineOfTrumps = player.Cards.FirstOrDefault(x => x.Type == CardType.Nine && x.Suit == deckState.TrumpCard.Suit);
+
+                if (nineOfTrumps != null)
+                {
+                    return new PlayerAction(PlayerActionType.ChangeTrump, deckState.TrumpCard);
+                }
+            }
+
+            return new PlayerAction(PlayerActionType.None);
+        }
+    }
+}
