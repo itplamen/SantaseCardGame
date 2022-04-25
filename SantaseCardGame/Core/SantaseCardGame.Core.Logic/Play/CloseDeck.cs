f1581ad2c3056453370ae3d03a4b137a0839c1fb
itplamen <itplamen@gmail.com>
2022-04-25 23:04:28 +0300

    Added playing action for closing the deck

---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Play/CloseDeck.cs
@@ -0,0 +1,39 @@
+﻿namespace SantaseCardGame.Core.Logic.Play
+{
+    using SantaseCardGame.Core.Logic.Contracts.Validators;
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class CloseDeck : BaseActionPlaying
+    {
+        private readonly IDeckState deckState;
+        private readonly IPlayerActionValidator playerActionValidator;
+
+        public CloseDeck(ITrickState trickState, IDeckState deckState, IPlayerActionValidator playerActionValidator)
+            : base(trickState)
+        {
+            this.deckState = deckState;
+            this.playerActionValidator = playerActionValidator;
+        }
+
+        public override PlayerActionResult Play(PlayerAction playerAction, Player player)
+        {
+            if (ShouldPlay(playerAction, player))
+            {
+                deckState.ShouldFollowSuit = true;
+                deckState.ClosedBy = player.Position;
+
+                return new PlayerActionResult(true);
+            }
+
+            return new PlayerActionResult(false);
+        }
+
+        protected override bool ShouldPlay(PlayerAction playerAction, Player player)
+        {
+            return base.ShouldPlay(playerAction, player) &&
+                playerActionValidator.CanCloseDeck(player) &&
+                playerAction.Type == PlayerActionType.CloseDeck;
+        }
+    }
+}
