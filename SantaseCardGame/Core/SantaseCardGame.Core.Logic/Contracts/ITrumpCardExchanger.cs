070ca569c2c27a66ad071450f9ac16a6dad9c861
itplamen <itplamen@gmail.com>
2022-04-25 16:33:40 +0300

    Created trump card exchanger

---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Contracts/ITrumpCardExchanger.cs
@@ -0,0 +1,9 @@
+﻿namespace SantaseCardGame.Core.Logic.Contracts
+{
+    using SantaseCardGame.Data.Models;
+
+    public interface ITrumpCardExchanger
+    {
+        void Exchange(Card trumpCard, Deck deck, Player player);
+    }
+}
---
+++ SantaseCardGame/Core/SantaseCardGame.Core.Logic/Exchanging/TrumpCardExchanger.cs
@@ -0,0 +1,32 @@
+﻿namespace SantaseCardGame.Core.Logic.Exchanging
+{
+    using System.Linq;
+
+    using SantaseCardGame.Core.Logic.Contracts;
+    using SantaseCardGame.Data.Models;
+    using SantaseCardGame.Infrastructure.States.Contracts;
+
+    public class TrumpCardExchanger : ITrumpCardExchanger
+    {
+        private readonly IDeckState deckState;
+
+        public TrumpCardExchanger(IDeckState deckState)
+        {
+            this.deckState = deckState;
+        }
+
+        public void Exchange(Card trumpCard, Deck deck, Player player)
+        {
+            Card nineOfTrumpsCard = player.Cards.First(x => x.Type == CardType.Nine && x.Suit == trumpCard.Suit);
+            int nineOfTrumpsIndex = player.GetCardPosition(nineOfTrumpsCard.Type, nineOfTrumpsCard.Suit);
+
+            player.RemoveCard(nineOfTrumpsCard);
+            player.AddCard(trumpCard, nineOfTrumpsIndex);
+
+            deck.RemoveCard(trumpCard);
+            deck.AddCard(nineOfTrumpsCard);
+
+            deckState.TrumpCard = nineOfTrumpsCard;
+        }
+    }
+}
