b4e36d37274529014d316c7465099561a2c6f0a5
itplamen <itplamen@gmail.com>
2022-07-21 01:35:21 +0300

    Improved the logic when AI decides to close the deck

--- SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/FirstPlayer/CloseDeckStrategy.cs
+++ SantaseCardGame/AI/SantaseCardGame.AI.Logic/Strategies/FirstPlayer/CloseDeckStrategy.cs
@@ -5,21 +5,23 @@
     using SantaseCardGame.AI.Logic.Contracts;
     using SantaseCardGame.Core.Logic.Contracts;
     using SantaseCardGame.Core.Logic.Contracts.Validators;
+    using SantaseCardGame.Data.Contracts;
     using SantaseCardGame.Data.Models;
     using SantaseCardGame.Infrastructure.States.Contracts;
 
     public class CloseDeckStrategy : IPlayerActionStrategy
     {
-        private const int ENOUGH_POINTS_REQUIRED_FOR_CLOSING = 50;
-        private const int REMAINING_POINTS_REQUIRED_FOR_CLOSING = 20;
+        private const int MIN_TRUMP_CARDS_REQUIRED_TO_CLOSE = 4;
 
         private readonly IGameState gameState;
+        private readonly IStorage<Game> gameStorage;
         private readonly IAnnouncementChecker announcementChecker;
         private readonly IPlayerActionValidator playerActionValidator;
 
-        public CloseDeckStrategy(IGameState gameState, IAnnouncementChecker announcementChecker, IPlayerActionValidator playerActionValidator)
+        public CloseDeckStrategy(IGameState gameState, IStorage<Game> gameStorage, IAnnouncementChecker announcementChecker, IPlayerActionValidator playerActionValidator)
         {
             this.gameState = gameState;
+            this.gameStorage = gameStorage;
             this.announcementChecker = announcementChecker;
             this.playerActionValidator = playerActionValidator;
         }
@@ -36,15 +38,23 @@
 
         private bool ShouldClose(Player player)
         {
-            return player.Points >= ENOUGH_POINTS_REQUIRED_FOR_CLOSING ||
-                HasEnoughPointsWithExistingCards(player) ||
+            return HasEnoughPointsWithExistingCards(player) ||
+                HasEnoughPointsWithExistingTrumpCards(player) ||
                 HasEnoughPointsWithExistingAnnouncements(player);
         }
 
         private bool HasEnoughPointsWithExistingCards(Player player)
         {
             return player.Points >= gameState.RoundHalfPoints && 
-                player.Cards.Sum(x => (int)x.Type) >= REMAINING_POINTS_REQUIRED_FOR_CLOSING;
+                player.Cards.Sum(x => (int)x.Type) >= gameState.RoundHalfPoints;
+        }
+
+        private bool HasEnoughPointsWithExistingTrumpCards(Player player)
+        {
+            var game = gameStorage.Get(gameState.CurrentGameId);
+
+            return player.Points >= gameState.RoundHalfPoints &&
+                player.Cards.Count(x => x.Suit == game.Deck.TrumpCard.Suit) >= MIN_TRUMP_CARDS_REQUIRED_TO_CLOSE;
         }
 
         private bool HasEnoughPointsWithExistingAnnouncements(Player player)
